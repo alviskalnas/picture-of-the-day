8571d1f56968a88df42e625721a0a326ec20483c
alviskalnas <alviskalnas@gmail.com>
2023-02-13 09:30:13 +0200

    picture-of-the-day

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector("#submitBtn").addEventListener("click", async function () {
+    const response = await fetch(
+      "https://api.nasa.gov/planetary/apod?api_key=pD9O2D8zLGhCMrEladF4veyAvNtOhdIgweodPaDl"
+    );
+    const data = await response.json();
+    document.querySelector("#title").innerHTML = data.title;
+    document.querySelector("#picture").src = data.url;
+    document.querySelector("#picture").alt = data.title;
+    document.querySelector("#explanation").innerHTML = data.explanation;
+    document.querySelector("#apod").style.display = "flex";
+  });
